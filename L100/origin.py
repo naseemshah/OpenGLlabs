e36f11fd6117078c320f663e2e28c688d1a7b31f
Naseem Shah <naseemshahvarikodan@gmail.com>
2020-08-08 12:35:46 +0530

    Initial Code: L100

---
+++ L100/origin.py
@@ -0,0 +1,30 @@
+from OpenGL.GL import*
+from OpenGL.GLU import*
+from OpenGL.GLUT import*
+import sys
+import math
+
+def init():
+    glClearColor(0.0,0.0,0.0,1.0)
+    gluOrtho2D(-1.0,1.0,-1.0,1.0)
+
+def plotpoints():
+    glClear(GL_COLOR_BUFFER_BIT)
+    glColor3f(1.0,0.0,0.0)
+    glPointSize(10.0)
+    glBegin(GL_POINTS)
+    glVertex2f(0.0,0.0)
+    glEnd()
+    glFlush()
+
+def main():
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB)
+    glutInitWindowSize(500,500)
+    glutInitWindowPosition(50,50)
+    glutCreateWindow("Plot Origin")
+    glutDisplayFunc(plotpoints)
+    init()
+    glutMainLoop()
+
+main()
---
+++ README.md
@@ -0,0 +1,17 @@
+# Naseem's OpenGLlabs
+Set of OpenGL programs experimenting the OpenGL library using Python and C++.
+
+# Logs
+### Latest first.
+
+- L100: Plot a point at origin
+    
+    Description: A program to plot an origin point.
+    + Language: Python
+    + Dependencies:
+        + PyOpenGL
+        + GLUT
+    + Directory: /L100
+    + Main file: origin.py
+
+    ![](/L100/ss.jpg)
